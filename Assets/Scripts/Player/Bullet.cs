d7cf86118ab76631cef0cf082a9f52e20cf7c91b
Ig0k <igik300@gmail.com>
2024-09-05 16:38:57 -0300

    NavMesh 2D agregado
    NavMesh 2D nos permite hacer pathfinding de forma facil para enemigos.

---
+++ Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _destroyTime;
+    [SerializeField] private int _damage;
+    [SerializeField] private LayerMask _layerMask;
+
+    [SerializeField] private bool _isPlayer = false;
+
+    public void SetProperties(float speed, float destroyTime, int damage)
+    {
+        _speed = speed;
+        _destroyTime = destroyTime;
+        _damage = damage;
+    }
+
+    private void Update()
+    {
+        transform.position += transform.up * _speed * Time.deltaTime;
+
+        Destroy(gameObject, _destroyTime);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, 0.5f, _layerMask);
+
+        if (_isPlayer)
+        {
+            if (hit.collider != null && hit.collider.TryGetComponent<EnemyPrototype>(out EnemyPrototype enemy1))
+            {
+                enemy1.TakeDamage(_damage);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+
+        }
+
+    }
+}
