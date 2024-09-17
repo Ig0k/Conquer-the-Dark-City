d5ccb049bc898b420f18be1541b7bf78b6186b11
luuuK22 <lukiacade22@gmail.com>
2024-09-17 02:22:37 -0300

    Meleé + Inicio lógica granada

---
+++ Assets/Scripts/Player/Grenade.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    [SerializeField]
+    private int grenadeAmount = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 7)
+
+        {
+            
+            
+
+                collision.GetComponent<PlayerGrenade>().AddGrenades(grenadeAmount);
+                Destroy(gameObject);
+                Debug.Log($"conseguiste {grenadeAmount} granada");
+            
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerGrenade.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrenade : MonoBehaviour
+{
+
+
+
+
+
+        [SerializeField]
+        private int grenadeAvailable;
+
+
+     
+
+
+        public void AddGrenades(int amount)
+        {
+            grenadeAvailable += amount;
+
+
+        }
+
+
+        public void RemoveGrenades(int amount)
+        {
+            if (Input.GetKeyDown(KeyCode.G)   &&grenadeAvailable > 0)
+            {
+                grenadeAvailable -= amount;
+
+               Debug.Log("tire una granada");
+            
+
+            }
+            else
+            {
+                Debug.Log("no tengo granadas");
+
+            }
+        }
+}
